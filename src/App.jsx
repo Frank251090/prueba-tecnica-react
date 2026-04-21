f196899175e9f4654c87ef0c576a0116622291c2
Javier Hernandez <desarrollo@grantour.app>
2026-04-20 19:57:51 -0600

    fix: configurar rutas en App.jsx para conectar el flujo completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// Importamos nuestras páginas
 import Step1Plan from './pages/Step1Plan';
 import Step2Data from './pages/Step2Data';
 import Step3Line from './pages/Step3Line';
 import Step4Portability from './pages/Step4Portability';
 import Step5Terms from './pages/Step5Terms';
 import Success from './pages/Success';
+
+import './App.css';
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
         <Route path="/linea" element={<Step3Line />} />
         <Route path="/portabilidad" element={<Step4Portability />} />
         <Route path="/terminos" element={<Step5Terms />} />
-        <Route path="/exito" element={<Success />} />
+        <Route path="/success" element={<Success />} />
       </Routes>
     </BrowserRouter>
   );
